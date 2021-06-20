00187b39f34ff9041abdb4900183bcb1816bfede
KhoiVu <vbnkhoi@gmail.com>
2021-06-20 16:17:09 +0700

    nop bai

--- getbitcoinprice/main.c
+++ getbitcoinprice/main.c
@@ -8,7 +8,7 @@ void main()
 	Socket_Client_Init("54.192.22.61", 80);
 	Send("GET /v1/bpi/currentprice.json HTTP/1.1\r\nHost: api.coindesk.com\r\n\r\n");
 	Recv(buffer, sizeof(buffer));
-	printf("Data from server: %s", buffer);
+	//printf("Data from server: %s", buffer);
 
 	char *key = "\"rate_float\":";
 	char *Position = strstr(key, &buffer);
